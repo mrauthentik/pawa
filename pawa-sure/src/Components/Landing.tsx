f734a3e3e12627ec64192fc175c6b86fda5be0f0
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-26 17:10:35 +0100

    enhance Landing component with framer-motion for image animation effects

--- pawa-sure/src/Components/Landing.tsx
+++ pawa-sure/src/Components/Landing.tsx
@@ -1,9 +1,30 @@
 import React from "react"
+import {motion} from "framer-motion"
+
 import img from '../assets/images/home-icon.png'
+
 const Landing: React.FC = () => {
 	return (
 		<div className="landing-container">
-			<img src={img} alt="" />
+			<motion.img 
+                alt="Pawasure image"
+                src={img}
+                animate={{
+                    opacity: [0.8, 1,0.8,2,0.8],
+                    filter: [
+                        "brightness(1) contrast(1)",
+                        "brightness(1.5) contrast(1.5)",
+                        "brightness(1) contrast(1)"
+                    ],
+                }}
+                transition={{
+                    repeat:Infinity,
+                    duration: 3,
+                    ease: "easeInOut",
+                }}
+            />
+
+            
 		</div>
 	);
 }
