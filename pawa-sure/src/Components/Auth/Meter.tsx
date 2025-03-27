877b6e24f6ab2488760793b98609fd20726ba620
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-27 16:58:35 +0100

    feat: update Meter component to use MUI Select for location selection with improved styling

--- pawa-sure/src/Components/Auth/Meter.tsx
+++ pawa-sure/src/Components/Auth/Meter.tsx
@@ -30,45 +30,57 @@ const Meter: React.FC = ()=>{
                 <input type="number" placeholder='Enter Your Meter Number' required/>
                
                 <label htmlFor="Location">Location</label>
-                <Select name="location" id="" required>
-                <option value="">Select a State</option>
-                <option value="abia">Abia</option>
-                <option value="adamawa">Adamawa</option>
-                <option value="akwa-ibom">Akwa Ibom</option>
-                <option value="anambra">Anambra</option>
-                <option value="bauchi">Bauchi</option>
-                <option value="bayelsa">Bayelsa</option>
-                <option value="benue">Benue</option>
-                <option value="borno">Borno</option>
-                <option value="cross-river">Cross River</option>
-                <option value="delta">Delta</option>
-                <option value="ebonyi">Ebonyi</option>
-                <option value="edo">Edo</option>
-                <option value="ekiti">Ekiti</option>
-                <option value="enugu">Enugu</option>
-                <option value="gombe">Gombe</option>
-                <option value="imo">Imo</option>
-                <option value="jigawa">Jigawa</option>
-                <option value="kaduna">Kaduna</option>
-                <option value="kano">Kano</option>
-                <option value="katsina">Katsina</option>
-                <option value="kebbi">Kebbi</option>
-                <option value="kogi">Kogi</option>
-                <option value="kwara">Kwara</option>
-                <option value="lagos">Lagos</option>
-                <option value="nasarawa">Nasarawa</option>
-                <option value="niger">Niger</option>
-                <option value="ogun">Ogun</option>
-                <option value="ondo">Ondo</option>
-                <option value="osun">Osun</option>
-                <option value="oyo">Oyo</option>
-                <option value="plateau">Plateau</option>
-                <option value="rivers">Rivers</option>
-                <option value="sokoto">Sokoto</option>
-                <option value="taraba">Taraba</option>
-                <option value="yobe">Yobe</option>
-                <option value="zamfara">Zamfara</option>
-                <option value="fct">Federal Capital Territory (Abuja)</option>
+                <Select name="location" 
+                        required
+                        placeholder="Please select Your Location"
+                        indicator={<KeyboardArrowDown/>}
+                        sx={{
+                            width:240,
+                            [`& .${selectClasses.indicator}`]:{
+                             transition: `0.2s`,
+                             [`& .${selectClasses.expanded}`]:{
+                                transform: `rotate(-180deg)`,
+                             },
+                            },
+                         }}
+                        >
+                <Option value="abia">Abia</Option>
+                <Option value="adamawa">Adamawa</Option>
+                <Option value="akwa-ibom">Akwa Ibom</Option>
+                <Option value="anambra">Anambra</Option>
+                <Option value="bauchi">Bauchi</Option>
+                <Option value="bayelsa">Bayelsa</Option>
+                <Option value="benue">Benue</Option>
+                <Option value="borno">Borno</Option>
+                <Option value="cross-river">Cross River</Option>
+                <Option value="delta">Delta</Option>
+                <Option value="ebonyi">Ebonyi</Option>
+                <Option value="edo">Edo</Option>
+                <Option value="ekiti">Ekiti</Option>
+                <Option value="enugu">Enugu</Option>
+                <Option value="gombe">Gombe</Option>
+                <Option value="imo">Imo</Option>
+                <Option value="jigawa">Jigawa</Option>
+                <Option value="kaduna">Kaduna</Option>
+                <Option value="kano">Kano</Option>
+                <Option value="katsina">Katsina</Option>
+                <Option value="kebbi">Kebbi</Option>
+                <Option value="kogi">Kogi</Option>
+                <Option value="kwara">Kwara</Option>
+                <Option value="lagos">Lagos</Option>
+                <Option value="nasarawa">Nasarawa</Option>
+                <Option value="niger">Niger</Option>
+                <Option value="ogun">Ogun</Option>
+                <Option value="ondo">Ondo</Option>
+                <Option value="osun">Osun</Option>
+                <Option value="oyo">Oyo</Option>
+                <Option value="plateau">Plateau</Option>
+                <Option value="rivers">Rivers</Option>
+                <Option value="sokoto">Sokoto</Option>
+                <Option value="taraba">Taraba</Option>
+                <Option value="yobe">Yobe</Option>
+                <Option value="zamfara">Zamfara</Option>
+                <Option value="fct">Federal Capital Territory (Abuja)</Option>
                 </Select>
                 <label htmlFor="provider">Select Your Service Provider/estate</label>
                 <Select name="provider" id="" required>
