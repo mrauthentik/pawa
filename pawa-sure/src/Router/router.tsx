0789d77b98f28b8434b635b793bfeaea489e3e31
mrauthentik <umokeuchenna2020@gmail.com>
2025-03-27 10:19:32 +0100

    add SignUp route to router for user registration

--- pawa-sure/src/Router/router.tsx
+++ pawa-sure/src/Router/router.tsx
@@ -1,11 +1,16 @@
 import {createBrowserRouter} from 'react-router-dom'
 import Landing from '../Components/Landing'
+import SignUp from '../Components/Auth/SignUp'
 
 const router = createBrowserRouter([
     {
         path:'/',
         element: <Landing />
-    }
+    },
+    {
+        path:'/signup',
+        element: <SignUp />
+    },
 ])
 
 export default router
