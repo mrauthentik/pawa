338992d80d9245c16849e690ff6eade87a84e110
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-02 13:47:43 +0100

    feat: enhance onboarding slider styles and functionality; adjust text box padding and add button navigation

--- pawa-sure/src/Components/onBoardingSlider.tsx
+++ pawa-sure/src/Components/onBoardingSlider.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import {useRef}from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import 'swiper/css'
 import 'swiper/css/pagination'
 import { Pagination,  } from "swiper/modules";
 import img1 from '../assets/images/slide1.png'
 import img2 from '../assets/images/slide4.png'
 import img3 from '../assets/images/slide3.png'
+import SwiperClass from "swiper";
 
 const slides = [
   {
@@ -31,15 +32,28 @@ const slides = [
 
 const OnboardingSlider = () => {
     const navigate = useNavigate()
+    const swiperRef = useRef<SwiperClass | null>(null)
   return (
     <Swiper pagination={{ clickable: true }} modules={[Pagination]} className="mySwiper">
       {slides.map((slide, index) => (
-        <SwiperSlide key={index} className="flex flex-col items-center justify-center">
+        <SwiperSlide key={index} className="flex flex-col items-center justify-center relative">
           <img src={slide.image} alt={slide.title} className="w-full h-60 object-cover" />
           <div className="p-4 bg-black-500 text-white text-center rounded-t-2xl text-box">
-            <h2 className="text-xl font-bold">{slide.title}</h2>
-            <p className="text-sm mt-2">{slide.description}</p>
-            <button className="mt-4 bg-yellow-500 text-black px-6 py-2 rounded-lg">{slide.buttonText}</button>
+            <h2 className=" font-bold">{slide.title}</h2>
+            <p className=" mt-2">{slide.description}</p>
+            <button 
+                className="mt-4 bg-yellow-500 text-black px-6 py-2 rounded-lg"
+                onClick={()=> {
+                    if(index === slides.length - 1){
+                        navigate('/signin'); 
+                    }else{
+                        swiperRef.current?.slideNext()
+                    }
+                    console.log('button is clicked')
+                }}
+           >
+                {slide.buttonText}
+            </button>
           </div>
         </SwiperSlide>
       ))}
