bfa93ccb25fbef838259bedf826c9eb386b485ca
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-07 17:00:14 +0100

    feat: update Dashboard component structure with initial content

--- pawa-sure/src/Components/DashBoard/Dashboard.tsx
+++ pawa-sure/src/Components/DashBoard/Dashboard.tsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 const Dashboard:React.FC = () => {
   return (
-    <div className='dashboard-container'>
-      <h1>Dashboard</h1>
-      <p>Welcome to dashboard</p>
+    <div >
+      
     </div>
   )
 }
