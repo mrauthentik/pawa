a764cb7fcc9558cbad0c16208d4213bf7df49935
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-07 16:56:55 +0100

    feat: add Dashboard component with initial structure

---
+++ pawa-sure/src/Components/DashBoard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard:React.FC = () => {
+  return (
+    <div className='dashboard-container'>
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
