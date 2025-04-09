c6a5eca192434c12372a6d8944aae273dbef1167
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-09 09:46:49 +0100

    feat: add TransactionHistory component for displaying transaction records

---
+++ pawa-sure/src/Components/History.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransactionHistory:React.FC = () => {
+    return(
+        <div className='transaction-history'>
+            <div className="header">
+                <h2>Transaction History</h2>
+                <p>View more </p>
+            </div>
+        </div>
+    )
+}
+
+export default TransactionHistory
