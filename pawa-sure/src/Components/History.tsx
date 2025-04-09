72effefb91c34ad236d2d5c93d5e6ac933995b2b
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-09 10:35:49 +0100

    feat: enhance Transaction History styles with background color and card design

--- pawa-sure/src/Components/History.tsx
+++ pawa-sure/src/Components/History.tsx
@@ -1,28 +1,39 @@
 import React from 'react'
 import img from '../assets/images/avatar.svg'
 
-const TransactionHistory:React.FC = () => {
-    return(
-        <div className='transaction-history'>
-            <div className="header">
-                <h2>Transaction History</h2>
-                <p>View more </p>
-            </div>
-            <div className="display-card">
-                <div className="details">
-                    <img src={img} alt="" />
-                    <div className="text">
-                        <h2>Shang-Li</h2>
-                        <p>3:31pm</p>
-                    </div>
-                </div>
-                <div className="amount">
-                    <h2> ₦5000</h2>
-                    <p>send</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+const transactions = [
+  { name: 'Shang-Li', time: '3:31pm', amount: '₦5000', type: 'send' },
+  { name: 'Marcus', time: '4:10pm', amount: '₦2000', type: 'receive' },
+  { name: 'Tina', time: '1:45pm', amount: '₦3500', type: 'send' },
+  { name: 'James', time: '5:20pm', amount: '₦1200', type: 'receive' },
+  { name: 'Jade', time: '6:00pm', amount: '₦8000', type: 'send' },
+];
 
-export default TransactionHistory
+const TransactionHistory: React.FC = () => {
+  return (
+    <div className='transaction-history'>
+      <div className="header">
+        <h2>Transaction History</h2>
+        <p>View more</p>
+      </div>
+
+      {transactions.map((txn, index) => (
+        <div className="display-card" key={index}>
+          <div className="details">
+            <img src={img} alt="" />
+            <div className="text">
+              <h2>{txn.name}</h2>
+              <p>{txn.time}</p>
+            </div>
+          </div>
+          <div className="amount">
+            <h2>{txn.amount}</h2>
+            <p>{txn.type}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TransactionHistory;
