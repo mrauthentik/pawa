afeb7a82a3f842ce278ba713f1d4468c5faab7ba
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-09 12:22:17 +0100

    feat: add PayModal component for payment processing

---
+++ pawa-sure/src/Components/PayModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ const PayModal: React.FC = () => {
+    return (
+        <div>
+            <h1>Payment Modal</h1>
+        </div>
+    ) 
+        
+}
+
+export default PayModal
