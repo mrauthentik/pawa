23a280ec1c2c53a2f0cb6948d2b5cf0805e0ba99
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-09 14:25:50 +0100

    feat: implement structured Payment page layout with options

--- pawa-sure/src/Components/Payment.tsx
+++ pawa-sure/src/Components/Payment.tsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Payment:React.FC = () =>{
     return (
-        <div>
-            Payment page
+        <div className='payment-page'>
+            <h1>Payment</h1>
+            <div className="amount">
+                <h2>₦45,000.00</h2>
+            </div>
+            <hr />
+
+            <p>Your saved cards</p>
+
+            <div className="bank">
+
+            </div>
+
+            <p>other payment methods</p>
+            
+            <div className="payment-options">
+                <div className="card">
+                    <div className="block">    
+                    <img src="" alt="" />
+                    <p>Pay with card</p>
+                    </div>
+                    <input type="radio" name='card' />
+                </div>
+                <div className="bank-transfer">
+                    <div className="block">
+                        <img src="" alt="" />
+                        <p>Bank Transfer</p> 
+                    </div>
+                    <input type="radio" name='card' />
+                </div>
+            </div>
+            
+            <button> Pay ₦10,000 </button>
         </div>
     )
 }
