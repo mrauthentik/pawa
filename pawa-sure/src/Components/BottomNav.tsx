2a4f5a9c7ba6b43089ed6e0cf43118b5cee1c8b4
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-10 10:50:33 +0100

    feat: add Link import for navigation in BottomNav component

--- pawa-sure/src/Components/BottomNav.tsx
+++ pawa-sure/src/Components/BottomNav.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
