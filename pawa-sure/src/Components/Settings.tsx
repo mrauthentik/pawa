86d5b5c8785d540e4f0484e4f1642400e2cdea1e
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-10 10:47:28 +0100

    feat: add settings list and user details to Settings component

--- pawa-sure/src/Components/Settings.tsx
+++ pawa-sure/src/Components/Settings.tsx
@@ -1,6 +1,15 @@
 import { UploadFile } from '@mui/icons-material'
 import React from 'react'
 
+const settings = [
+    {img:'', title:'Edit Profile', icon:''},
+    {img:'', title:'Notification', icon:''},
+    {img:'', title:'Language', icon:''},
+    {img:'', title:'Security', icon:''},
+    {img:'', title:'Dark Mode', icon:''},
+    {img:'', title:'About', icon:''},
+]
+
 const Settings:React.FC = ()=> {
     return (
         <div className='settings'>
@@ -14,6 +23,26 @@ const Settings:React.FC = ()=> {
                     <UploadFile />
                 </div>
             </div>
+            <div className="details">
+                <div className="name">Charles Babbage</div>
+                <div className="email">
+                 <p>charles_babbage12@gmail.com</p>
+                </div>
+            </div>
+            <div className="sets">
+            {settings.map((items,index)=>(
+                <div key={index}>
+                    <div className="name">
+                        <img src={items.img} alt="" />
+                        <p>{items.title}</p>
+                    </div>
+                    <div className="icon">
+                        {items.icon}
+                    </div>
+                </div>
+            ))}
+            </div>
+            <button> Log Out</button>
         </div>
     )
 }
