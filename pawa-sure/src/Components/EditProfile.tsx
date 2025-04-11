0d63135002542b57c64da92dffb50a6abb8f3c26
mrauthentik <umokeuchenna2020@gmail.com>
2025-04-11 12:34:52 +0100

    feat: Implement EditProfile component with image upload functionality

--- pawa-sure/src/Components/EditProfile.tsx
+++ pawa-sure/src/Components/EditProfile.tsx
@@ -1,12 +1,21 @@
+import { Cancel } from '@mui/icons-material'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import avatar from "../assets/images/profile-img.svg"
+import upload from '../assets/images/upload.svg'
 
 
 const EditProfile:React.FC = () => {
     const navigate = useNavigate()
     return (
         <div className='edit-profile'>
-             <div className="cancel"></div>
+             <div className="cancel">
+                <Cancel />
+                <h2>Edit Profile</h2>
+             </div>
+             <div className="img-holder">
+                <img src={avatar} alt="" />
+             </div>
         </div>
     )
 }
